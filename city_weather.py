671b7ea3b9f83987aac7c4f9adf0cfcaa6bee39c
saranraj <saranraj.jayaraj@ericsson.com>
2019-06-25 16:25:42 +0100

    Variable correction

--- city_weather.py
+++ city_weather.py
@@ -4,21 +4,21 @@ import pandas as pd
 
 sns.set()
 #Load the CSV file
-summarySheet = pd.read_csv('./data/mumbai.csv')
+df = pd.read_csv('./data/mumbai.csv')
 
 #List used to retain the month order
 months = ["December", "November", "October", "September",
           "August","July", "June", "May", "April", "March", "February", "January"]
 
 #load only required columns
-skipColDf = summarySheet[["Month","Year","temp"]]
+skipColDf = df[["Month","Year","temp"]]
 
 #calculate average temperature by month & Year
-day1Summary = skipColDf.groupby(["Month","Year"], as_index=False).mean()
-day1Summary.temp = day1Summary.temp.astype('int64')
+groupDf = skipColDf.groupby(["Month","Year"], as_index=False).mean()
+groupDf.temp = groupDf.temp.astype('int64')
 
-day1Summary['Month'] = pd.Categorical(day1Summary['Month'], categories=months, ordered=True)
-summary = day1Summary.pivot("Month", "Year", "temp")
+groupDf['Month'] = pd.Categorical(groupDf['Month'], categories=months, ordered=True)
+summary = groupDf.pivot("Month", "Year", "temp")
 
 #Draw heatmap
 f, ax = plt.subplots(figsize=(9, 6))
